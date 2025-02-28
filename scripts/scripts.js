758fa0e0af2f856507048c7efac20d67126f26f9
Krists Krievins <krists.krievins@gmail.com>
2025-02-28 19:18:15 +0100

    It's Friday

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,6 +4,7 @@ let musicTimer;
 musicTimer = setTimeout(() => {
   let backgroundMusic = new Audio("./music/start-screen-bmg.mp3");
   backgroundMusic.volume = 0.4;
+  backgroundMusic.loop = true;
   backgroundMusic.play();
 }, 500);
 
@@ -13,10 +14,10 @@ const menuButtons = document.getElementById("menuButton");
 
 // Functions
 
-// menuButtons.addEventListener("mouseover", () => {
-//   menuButtons.style.filter = "drop-shadow(0px 0px 5px rgb(0, 0, 0))";
-// });
+menuButtons.addEventListener("mouseover", () => {
+  menuButtons.style.filter = "drop-shadow(0px 0px 5px rgb(0, 0, 0))";
+});
 
-// menuButtons.addEventListener("mouseout", () => {
-//   menuButtons.style.filter = "drop-shadow(0px 0px 3px rgb(0, 0, 0))";
-// });
+menuButtons.addEventListener("mouseout", () => {
+  menuButtons.style.filter = "drop-shadow(0px 0px 3px rgb(0, 0, 0))";
+});
