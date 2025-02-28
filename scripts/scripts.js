5a2a649a7b2499a0e28a0c3c977a2aae27c1db53
Krists Krievins <krists.krievins@gmail.com>
2025-02-28 19:05:05 +0100

    Menu Screen V1

---
+++ scripts/scripts.js
@@ -0,0 +1,18 @@
+// Audio
+let backgroundMusic = new Audio("./music/start-screen-bmg.mp3");
+backgroundMusic.volume = 0.4;
+backgroundMusic.play();
+
+// Elements
+
+const menuButtons = document.getElementById("menuButton");
+
+// Functions
+
+// menuButtons.addEventListener("mouseover", () => {
+//   menuButtons.style.filter = "drop-shadow(0px 0px 5px rgb(0, 0, 0))";
+// });
+
+// menuButtons.addEventListener("mouseout", () => {
+//   menuButtons.style.filter = "drop-shadow(0px 0px 3px rgb(0, 0, 0))";
+// });
